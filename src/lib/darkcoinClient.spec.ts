d69f470ac9aad47a86befeeb6fc14cd786e5362e
DashUser33 <dashuser33@protonmail.com>
2018-09-16 13:51:40 -0300

    add tests against a RPC instance
    Add a first proof of concept test

--- src/lib/darkcoinClient.spec.ts
+++ src/lib/darkcoinClient.spec.ts
@@ -1,13 +1,28 @@
 import test from 'ava';
+import validator from 'wallet-address-validator';
 import { DarkcoinClient } from './darkcoinClient';
 
+const user = process.env.DASHD_USER;
+const password = process.env.DASHD_PASSWORD;
+const url = process.env.DASHD_URI;
+const client = new DarkcoinClient({
+  url,
+  user,
+  password
+});
+
+/**
+ * This test suite requires an instance of DashD.
+ * DO NOT USE MAINNET or you might lose funds.
+ */
+test.serial('Fail if required env vars are not defined', t => {
+  t.truthy(user);
+  t.truthy(password);
+  t.truthy(url);
+});
+
 test('Fail on undefined parameters', async t => {
-  const config = new DarkcoinClient({
-    url: 'http://localhost:8887',
-    user: 'rpcuser',
-    password: 'secret'
-  });
-  await config
+  await client
     .sendToAddress('foo', undefined, 'bar')
     .then(r => t.fail('Should not have a result: ' + r))
     .catch(e => {
@@ -17,4 +32,12 @@ test('Fail on undefined parameters', async t => {
           e.message === 'Undefined arguments found after defined arguments.'
       );
     });
+});
+
+test('get a receiving address', async t => {
+  await client.getNewAddress().then(r => {
+    const validationResult = validator.validate(r.result, 'dash', 'testnet');
+
+    t.is(validationResult, true);
+  });
 });
