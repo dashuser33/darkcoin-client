74edfa507963290eb6c1a965698422562661e361
Jeff Hodges - Desktop <jeffhodges93@gmail.com>
2018-09-20 21:26:55 -0700

    Added dotenv support. Fixed type error due to upstream change.

--- src/lib/darkcoinClient.spec.ts
+++ src/lib/darkcoinClient.spec.ts
@@ -1,10 +1,20 @@
 import { serial as test } from 'ava';
+import * as dotenv from 'dotenv';
 import validator from 'wallet-address-validator';
 import { DarkcoinClient } from './darkcoinClient';
+
+// Try to load in a .env if there is one. Otherwise, continue.
+try {
+  dotenv.config();
+} catch (err) {
+  console.log(err);
+  console.log('No .env file found, loading ENV variables from process.');
+}
 
 const user = process.env.DASHD_USER;
 const password = process.env.DASHD_PASSWORD;
 const url = process.env.DASHD_URI;
+
 const client = new DarkcoinClient({
   url,
   user,
